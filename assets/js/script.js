bcc3bae608115e548ddf8c2207a472870c48d2f9
rschragger <reveschragger@gmail.com>
2022-06-04 17:59:30 +1000

    220604 18:00 Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+  y= "Y" ;
+  return y ;
+}
+
 // Write password to the #password input
 function writePassword() {
+  //Prompt for password length
+  //prompt for character types
+  
+  
+  //create password
   var password = generatePassword();
+  console.log (password)
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
+document.getElementById("generate").addEventListener("click", writePassword);
 
-console.log(
-  generateBtn + "\n" +
-password  )
