f78f9008c8ed0790fd66901a1dbad8bf32fea73d
rschragger <reveschragger@gmail.com>
2022-06-06 15:07:54 +1000

    220606 15:00 Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,34 @@
+//Create variables
+const UCaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+const LCaseList = "abcdefghijklmnopqrstuvwxyz" ;
+const NumbsList = "0123456789" ;
+const SymbsList = "~!@#$%^&*()[]{}-+=|\<>?/" ;
+
+var CharcTypes = {
+  UCaseLett : true ,
+  LCaseLett : true ,
+  Numbs : true ,
+  Symbs : true ,
+};
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Get Character Types to use
 
+//Generate a random character
+function generateRandChar (CharcTypeChoice){
+  var numInList = CharcTypeChoice.length ;
+  randNo = Math.floor ( Math.random() * numInList ) ;
+  NewChar = CharcTypeChoice.substring(randNo,randNo+1);
+  console.log (randNo) ;
+  console.log (NewChar) ;
+  return NewChar ;
+  }
+
+
+//***This is the maths function */
 function generatePassword() {
   y= "Y" ;
   return y ;
@@ -11,6 +38,7 @@ function generatePassword() {
 function writePassword() {
   //Prompt for password length
   //prompt for character types
+
   
   
   //create password
