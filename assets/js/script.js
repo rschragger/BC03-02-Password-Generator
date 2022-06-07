958eb094bed422a438fd07106d5bbe84613e959e
rschragger <reveschragger@gmail.com>
2022-06-08 01:17:59 +1000

    220608 01:17 Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,28 +22,81 @@ function generateRandChar(charcTypeChoice) {
   return NewChar;
 }
 
+var promptTypeList = function () {
+  //prompt for character types
+  //Make an array with possible values [ uCaseLett lCaseLett numbs symbs ] 
+  var charTypeList = [];
+  var charChoiceList = ["uCaseLett", "lCaseLett", "numbs", "symbs"];
+  var messageList = ["Uppercase (A...Z)", "Lowercase (a...z)", "Numbers (0...9)", "Symbols (!@#$...)"];
+  var isCorrect = false;
 
-//***This is the maths function */
+  //cycle through the choices of character types
+  for (var charChoice = 0; charChoice < charChoiceList.length; charChoice++) {
+    isCorrect = false
+    while (isCorrect != true) {
+      var choice = window.prompt("Do you want " + messageList[charChoice] + "in your password?", "Y/N");
+      choice = choice.toUpperCase();
+      console.log(choice + "   " + charChoice);
+      if (choice == "Y" || choice == "N") {
+        isCorrect = true;
+        if (choice == "Y") {
+          charTypeList.push(charChoiceList[charChoice]);
+          console.log(charTypeList);
+        }
+      } else {
+        isCorrect = false;
+        alert("Please check your answer");
+      }
+    }
+  }
+  return charTypeList
+} //end promptTypeList()
+
+//Prompt for password length
+  promptPasswordLength = function () {
+    
+    var isCorrect = false;
+    while (isCorrect != true) {
+      var passwordLengthAsk = window.prompt("How many characters do you want in your password?", "Enter a number between 8 and 128");
+      if (passwordLengthAsk < 8 || passwordLengthAsk > 128 || isNaN(passwordLengthAsk )) {
+        isCorrect = false;
+        alert("Please check your answer");
+      } else {
+        isCorrect = true;
+      }
+    }
+    return passwordLengthAsk ;
+  } //end promptPasswordLength()
+
+
+
+
+//*** generatePassword() - This is the maths function */
 function generatePassword() {
-  // charcTypeList()
-  // console.log("charcTypes: " + charcTypeList())
+  var PasswordLength = promptPasswordLength() ;
+  var charTypeArray = promptTypeList() ;
+console.log( PasswordLength )
+console.log( charTypeArray);
+
   y = "Y";
-  return y;
+  // return y;
+  return PasswordLength + " + " + charTypeArray;
 }
 // Write password to the #password input
 function writePassword() {
-   //Prompt for password length
-  //prompt for character types
+
+
+  
 
 
 
   //create password
-   var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+} //end writePassword() *********
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
